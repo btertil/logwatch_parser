5133dddc2289ad0f6eed0eb26f2c175893b00d38
bartek <bondiev@gmail.com>
2020-11-15 22:47:25 +0100

    prototyp listy maili Logwatch

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ import os
 
 httpd_begin = " --------------------- httpd Begin ------------------------"
 httpd_end = " ---------------------- httpd End -------------------------"
-#        /
+scan_line = "       /"
 sshd_begin = " --------------------- SSHD Begin ------------------------"
 sshd_end = " ---------------------- SSHD End -------------------------"
 
@@ -21,3 +21,15 @@ eml_path = "home/bartek/eclipse-workspace/logwatch_parser/eml/hrankiety/*.eml"
 lista = os.listdir("eml/hrankiety/")
 print(len(lista))
 print(lista[:5])
+
+counter = 0
+for l in lista:
+    flag = False
+    if "Logwatch for " in l:
+        flag = True
+        print(l, flag)
+        counter += 1
+    else:
+        pass
+    
+print(counter)
