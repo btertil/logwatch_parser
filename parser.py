a2e93913ff3635f4d98540350744929bba4e25c7
bartek <bondiev@gmail.com>
2020-11-18 22:00:54 +0100

    identyfikacja ip przez ip pattern i regex

--- parser.py
+++ parser.py
@@ -10,20 +10,21 @@ scan_line = "       /"
 sshd_begin = " --------------------- SSHD Begin ------------------------"
 sshd_end = " ---------------------- SSHD End -------------------------"
 
-https_flag = False
-sshd_flag = False
-
 eml_path = "home/bartek/eclipse-workspace/logwatch_parser/eml/hrankiety/*.eml"
+
+ip_pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+date_pattern = r"\d{4}\-d{2}\-d{2}"
 
 #import glob
 #print(glob.glob(eml_path))
 
 #lista = os.listdir("eml/hrankiety/")
 lista = os.listdir("eml/backup/")
-print(len(lista))
-print(lista[:5])
+#print(len(lista))
+#print(lista[:5])
 
 counter = 0
+
 for el in lista:
     flag = False
     if "Logwatch for " in el:
@@ -31,39 +32,42 @@ for el in lista:
         #print(el, flag)
         counter += 1
         
-        if counter < 3:
+        if counter < 11:
             with open("./eml/backup/{}".format(el), "r") as file:
                 lines = file.read()
                 
                 # re-setting flags for a new file / message
-                httpd_flag_begin_in_lines = False
-                httpd_flag_end_in_lines = False
-                sshd_flag_begin_in_lines = False
-                sshd_flag_end_in_lines = False
+                httpd_flag = False
+                sshd_flag = False
                 
+                httpd_ips = []
+                sshd_ips = []
+                
+                
+                # parse log messages line by line
                 for line in lines.split("\n"):
                     if line not in (""," ", "\n"):
-                        if sshd_flag_begin_in_lines == False and sshd_begin in line:
-                            sshd_flag_begin_in_lines = True
-                            sshd_flag_end_in_lines = False
-                        if sshd_flag_end_in_lines == False and sshd_end in line:
-                            sshd_flag_end_in_lines = True
                         
-                        print(line, sshd_flag_begin_in_lines, sshd_flag_end_in_lines)
+                        # is it httpd line?
+                        if httpd_flag == False and httpd_begin in line:
+                            httpd_flag = True
+                        if httpd_flag == True and httpd_end in line:
+                            httpd_flag = False
+                            
+                        # is it sshd line?    
+                        if sshd_flag == False and sshd_begin in line:
+                            sshd_flag = True
+                        if sshd_flag == True and sshd_end in line:
+                            sshd_flag = False
+                            
+                        if httpd_flag:
+                            httpd_ips +=  re.findall(ip_pattern, line)
+                            
+                        if sshd_flag:
+                            sshd_ips +=  re.findall(ip_pattern, line)    
+                        
+                        #print(line, httpd_flag, sshd_flag)
+                
+                print("\n\nfile: {}\n\thttpd probing ips: {}\n\tssh succesull logins: {}".format(el, ", ".join(httpd_ips), ", ".join(sshd_ips)))
                     
-                    #if sshd_begin in lines:
-                    #    print("sshd_begin in lines")
-                    #print(el)
-                    #print(lines)
-                    #lines = lines.split("\n")
-                    #if sshd_begin in lines:
-                    #    print("sshd_begin in lines")
-                    #for line in lines:
-                           #if line.contains(sshd_begin):
-                           #    print("line == sshd_begin")
-                           #    print(el, line)
-        
-    else:
-        pass
-    
-print(counter)
+
